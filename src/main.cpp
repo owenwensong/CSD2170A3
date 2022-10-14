b22101528b63e91205e7dbf1a5bd53edefce9959
owenwensong <w.huang@digipen.edu>
2022-10-14 21:31:25 +0800

    closer near plane

--- src/main.cpp
+++ src/main.cpp
@@ -82,7 +82,7 @@ public:
 		camera.type = Camera::CameraType::lookat;
 		camera.setPosition(glm::vec3(0.0f, 0.0f, -2.0f));
 		camera.setRotation(glm::vec3(0.0f));
-		camera.setPerspective(60.0f, (float)width * 0.5f / (float)height, 1.0f, 256.0f);
+		camera.setPerspective(60.0f, (float)width * 0.5f / (float)height, 0.125f, 256.0f);
 	}
 
 	~VulkanExample()
